853a94bcdb956f4469f4eeef4fdec20233f06d0f
Ben Cook <benthomascook28@gmail.com>
2017-09-16 13:08:57 -0400

    removed defaults in head

--- index.php
+++ index.php
@@ -8,8 +8,6 @@
 <html lang="en">
     <head>
         <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link href="https://fonts.googleapis.com/css?family=Coda" rel="stylesheet">
         <link rel="stylesheet" href="/css/grid.css" media="screen" title="no title" charset="utf-8">
         <title>Badger</title>
